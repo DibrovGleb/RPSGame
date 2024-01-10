acdcb814614f6c85b332ee4e9e527cbdd7bc1553
Dibrov Gleb <g.t.ru@mail.ru>
2024-01-10 11:45:07 +0300

    winner animation

--- script.js
+++ script.js
@@ -18,11 +18,28 @@ class Game {
     static losestreak = 0;
     static tiestreal = 0;*/
 
-    static play (userChoise,aiChoise) {
-        if (userChoise == aiChoise) {
-            return this.score(2)
-        }
-        else {
+    static fight(elem){
+        this.iconsAnimation(1)
+
+        setTimeout(() => {
+                let aires = this.getAiChoise()
+
+                iconsComp[aires].style.cssText = "display: block; opacity:1;"
+                elem.target.style.cssText = "display: block; opacity:1; pointer-events: none;"
+                setTimeout(() => {
+                    this.animateWin(this.play(elem.target.id, aires), iconsComp[aires], elem.target)
+                    this.displayScore()
+                    setTimeout(() => {
+                        this.newgame()
+                    }, 1500);
+                }, 200)
+        }, 1000)
+    }
+
+    // determine the winner and return the value
+    static play (userChoise, aiChoise) {
+        if (userChoise == aiChoise) return this.score(2)
+        else
             switch (userChoise) {
                 case '0':
                 return aiChoise == 1 ? this.score(1) : this.score(0)
@@ -31,9 +48,9 @@ class Game {
                 default:
                 return aiChoise == 0 ? this.score(1) : this.score(0)
             }
-        }
     }   
     
+    // Adds the result to the desired variable changes the robot icon and returns result
     static score (result){
         this.gamesCount++
         result == 2 ? this.tie++ + this.animateRobot('what', 'moveup')
@@ -42,6 +59,7 @@ class Game {
         return result
     }
 
+    // Update score
     static displayScore(){
         wins.innerText = this.wins
         loses.innerText = this.lose
@@ -49,36 +67,31 @@ class Game {
         total.innerText = this.gamesCount
     }
 
-    static fight(elem){
-        this.iconsAnimation(1)
-
-        setTimeout(() => {
-                let aires = Game.getAiChoise()
-
-                iconsComp[aires].style.cssText = "display: block; opacity:1;"
-                elem.target.style.cssText = "display: block; opacity:1; pointer-events: none;"
-                setTimeout(() => {
-                    this.animateWin(this.play(elem.target.id, aires), iconsComp[aires], elem.target)
-                    Game.displayScore()
-                    setTimeout(() => {
-                        Game.newgame()
-                    }, 1500);
-                }, 300)
-        }, 1000)
-    }
-
+    // Generate random number from 0 to 2 where 0 is rock and 1 is scissors
     static getAiChoise(){
         return Math.round(Math.random()*2)
     }
 
+    // Starting new game by showing up the icons
     static newgame(){
         this.iconsAnimation(0)
     }
 
     
-    static animateWin(winner){
-        //winner.style.cssText = "transform: scale(3.5) rotateY(180deg)"
-        console.log(winner)
+    static animateWin(winner, compicn, playericn){
+        switch (winner) {
+            case 1:
+                playericn.style.cssText = "transform: scale(4.5) rotateY(360deg)"
+                break;
+            case 0:
+                compicn.style.cssText = "transform: rotateX(180deg) scale(4.5) rotateY(-360deg)"
+                break
+            default:
+                playericn.style.cssText = "transform: scale(4.5) rotateY(360deg)"
+                compicn.style.cssText = "transform: rotateX(180deg) scale(4.5) rotateY(-360deg)"
+                break;
+        }
+        console.log(winner, compicn, playericn)
     }
 
     // Robot icon animation
@@ -113,7 +126,7 @@ class Game {
             iconsComp.forEach(icncomp => {
                 icncomp.style.display = 'block'
                 setTimeout(() => {
-                    icncomp.style.opacity = '1'
+                    icncomp.style.cssText = 'opacity:1;'
                 }, 500);
             })
             iconsPlayer.forEach(icn => {
@@ -131,4 +144,3 @@ iconsPlayer.forEach(element => {
         Game.fight(elem)
     })
 });
-
